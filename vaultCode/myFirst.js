1bdf685269813c1670c7a873b49d8b6acd394aed
PatrickW84 <allenpatrickwilliams@gmail.com>
2024-05-23 12:51:09 -0500

    vaultCode

---
+++ vaultCode/myFirst.js
@@ -0,0 +1,19 @@
+alert("Welcome to 127.0.0.1");
+
+// Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog.
+
+// Assign three variables. Each variable contains the corresponding result of
+// calculation using a unique arithmetic operator(+-*/).
+
+let num1 = "0 + 10";
+let num2 = "-80 / 2";
+let num3 = "-46 + 7";
+
+// create window alerts when someone visits the page containing the string
+// then containing mathematical problems to solve for the combination.
+alert(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+);
+alert(num1);
+alert(num2);
+alert(num3);
